363ca618514d5f467ab63761d03dc70371609be9
IcksLin <icks49003@gmail.com>
2025-11-04 18:08:44 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// 前向声明
+//链表======================================
+// 前向声明，节点
 typedef struct MyNode MyNode;
 // 结构体定义
 struct MyNode {
     int data;
     MyNode* next;
 };
-
-typedef struct MyTreeNode {
-    int data;
-    struct MyNode* left;
-    struct MyNode* right;
-}MyTreeNode;
-
 void node_init(MyNode** root);
 void add_node(MyNode** root , int data);
 void delete_node(MyNode** root, int data);
 void print_list(MyNode* root);
+//==========================================
 
+//BST（查找二叉树）============================
+//前向声明，二叉树（查找二叉树实现）
+typedef struct BST_node BST_node;
+//结构体定义
+struct BST_node {
+    int data;
+    BST_node* left;
+    BST_node* right;
+};
+
+void BST_insert(BST_node** root,int data);
+BST_node* BST_find(BST_node* root,int data);
+void BST_delete_tree(BST_node** root, int data);
+void BST_print_list(BST_node* root);
+//===========================================
 
 int main() {
+    //链表测试=================================
     MyNode* root = NULL;
-    // 直接初始化，不要先指向栈变量
     node_init(&root);
     for (int i = 0; i < 10; i++) {
         add_node(&root, i);
@@ -32,6 +43,20 @@ int main() {
     delete_node(&root, 20);
     delete_node(&root, 2);
     print_list(root);
+    //BST测试=================================
+    printf("\n===============================\n");
+    BST_node* BST_root = NULL;
+    BST_insert(&BST_root, 20);
+    for (int i = 0; i<100;i++) {
+        BST_insert(&BST_root, i);
+    }
+    BST_print_list(BST_root);
+    BST_node *find_test = BST_find(BST_root, 70);
+    if (find_test != NULL) {
+        printf("\n Test Successful! \n");
+        printf(" Test Data : ");
+        printf("%d", find_test->data);
+    }
     return 0;
 }
 
@@ -107,4 +132,68 @@ void print_list(MyNode* root) {
         printf("%d ", root->data);
 
     }
+}
+
+//BST树的递归插入
+void BST_insert(BST_node** root,int data){
+    if (root == NULL) {
+        return; // 无效的根指针
+    }
+
+    if (*root == NULL) {
+        // 创建新节点
+        *root = (BST_node*)malloc(sizeof(BST_node));
+        if (*root == NULL) {
+            printf("Memory allocation failed\n");
+            return;
+        }
+
+        // 初始化新节点
+        (*root)->data = data;
+        (*root)->left = NULL;
+        (*root)->right = NULL;
+        printf("New node inserted with data: %d\n", data);
+        return;
+    }
+
+    if ((*root)->data > data) {
+        BST_insert(&(*root)->left, data);
+    }else if ((*root)->data < data) {
+        BST_insert(&(*root)->right, data);
+    }
+}
+
+//递归打印
+void BST_print_list(BST_node* root) {
+    if (root == NULL) {
+        return;
+    }
+    printf("%d ", root->data);
+    if (root->left != NULL) {
+        BST_print_list(root->left);
+    }
+    if (root->right != NULL) {
+        BST_print_list(root->right);
+    }
+}
+
+//查找
+BST_node* BST_find(BST_node* root,int data) {
+    BST_node* temp = root;
+    if (temp==NULL) {
+        printf("Node not found\n");
+        return NULL;
+    }
+    while (temp != NULL) {
+        if (temp->data == data) {
+            return temp;
+        }
+        if (temp->data > data) {
+            temp = temp->left;
+        }else if (temp->data < data) {
+            temp = temp->right;
+        }
+    }
+    printf("Node not found\n");
+    return NULL;
 }
